571215e6f5dc82b78b63d01c6d3ab055fbc89832
alexanderglemme <alexander.glemme@gmail.com>
2023-04-23 13:40:04 +0000

    Add index view, make templates, add statics

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import SalesAd
 
 
+class SalesAdList(generic.ListView):
+    model = SalesAd
+    queryset = SalesAd.objects.filter(sold=False).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
