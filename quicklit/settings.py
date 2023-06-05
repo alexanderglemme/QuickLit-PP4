803cc97c2d4d6baca5a6e9aec3441734ad2cd2f3
alexanderglemme <alexander.glemme@gmail.com>
2023-06-05 17:13:29 +0000

    Turn debug to false

--- quicklit/settings.py
+++ quicklit/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['quicklit-pp4.herokuapp.com', '8000-alexandergl-quicklitpp4-qkcnv1e51zg.ws-eu98.gitpod.io', 'localhost']
 
